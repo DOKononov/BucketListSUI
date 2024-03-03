88c88e8e1d0a6c49f4ba81f576d31417ccd4976a
d.o.kononov <d.o.kononov@gmail.com>
2024-03-03 10:11:17 +0300

    Initial Commit

---
+++ BucketListSUI/BucketListSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListSUIApp.swift
+//  BucketListSUI
+//
+//  Created by Dmitry Kononov on 3.03.24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
